d73022c7498dea11960f8cb8bda0b95d26a26e2c
2000080172 <81341236+2000080172@users.noreply.github.com>
2021-08-03 16:09:38 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+//Initialize an integer array with ascii values and print the corresponding character values in a single row//
+public class Main 
+{  
+public static void main(String[] String)   
+{  
+int ch1 = 'a';  
+int ch2 = 'b';  
+System.out.println("The ASCII value of a is: "+ch1);  
+System.out.println("The ASCII value of b is: "+ch2);  
+}  
+}  
